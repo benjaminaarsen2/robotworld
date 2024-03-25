9c40229dfe6ced67f8fc139f6425b3253adcd423
WoutNagtegaal <117655924+WoutNagtegaal@users.noreply.github.com>
2024-03-25 14:54:05 +0100

    Update Robot.cpp

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -508,7 +508,7 @@ void Robot::drive() {
 				driving = false;
 
 				Model::RobotWorld::getRobotWorld().deleteWayPoint(getOutOfMyWayPoint);
-				std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				while(Model::RobotWorld::getRobotWorld().getWayPoint("WP")) {}
 				if(!goal) {
 					goal = Model::RobotWorld::getRobotWorld().getGoal("Goal");
 				}
